c9a595da593899dd850d44f2de38901852842244
Chris Skogstad <chris.skogstad1@gmail.com>
2020-11-01 23:54:21 +1100

    basic handlebars to test

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,7 +4,15 @@ var router = express.Router();
 
 var burgers = require("../models/burgers.js");
 
-
+router.get("/", function (req, res) {
+    cat.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 
 
