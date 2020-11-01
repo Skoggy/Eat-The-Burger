d95fe1400dc7a4ccb3f08f32f1a12d00fbb89116
Chris Skogstad <chris.skogstad1@gmail.com>
2020-11-01 23:10:14 +1100

    initial setup of folders

---
+++ config/orm.js
@@ -0,0 +1,50 @@
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+
+var orm = {
+    selectAll: function () {
+
+    },
+
+
+    insertOne: function () {
+
+    },
+
+
+    updateOne: function () {
+
+    }
+}
+
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+
+
+USE burgers_db;
+
+INSERT INTO burgers 
+VALUES ("Big Mac", "true"),
+ ("Double Cheeseburger", "false"), 
+ ("Hamburger", "true");
