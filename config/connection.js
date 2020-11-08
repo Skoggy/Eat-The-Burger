b1d029e41d4534742530ee45907619ef0ae528f4
Chris Skogstad <chris.skogstad1@gmail.com>
2020-11-08 20:38:39 +1100

    fixing connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -7,22 +7,22 @@ if (process.env.JAWSDB_URL) {
 } else {
 
     var connection = mysql.createConnection({
-        host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "localhost",
         port: 3306,
-        user: "dp2feb23hvysf7mr",
-        password: "j5qgzz2llnkgi2xs",
-        database: "qbv67xkza3bkt6w5"
+        user: "root",
+        password: "christopher",
+        database: "burgers_db"
     })
 }
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+// connection.connect(function (err) {
+//   if (err) {
+//      console.error("error connecting: " + err.stack);
+//       return;
+//  }
+//   console.log("connected as id " + connection.threadId);
+//});
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
